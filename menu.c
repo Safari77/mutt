e1eb556317d1e7a97a16c5d4bf7ef1d1e86b80d0
Sami Farin <hvtaifwkbgefbaei@gmail.com>
2026-07-14 15:36:07 +0300

    menu_pad_string : ensure cols >= 0

--- menu.c
+++ menu.c
@@ -223,7 +223,7 @@ static void menu_pad_string(MUTTMENU *menu, char *s, size_t n)
 {
   char *scratch = safe_strdup(s);
   int shift = option(OPTARROWCURSOR) ? 3 : 0;
-  int cols = menu->indexwin->cols - shift;
+  int cols = MAX(0, menu->indexwin->cols - shift);
 
   mutt_format_string(s, n, cols, cols, FMT_LEFT, ' ', scratch, mutt_strlen(scratch), 1);
   s[n - 1] = 0;
