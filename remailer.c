9cbd7adde466648cc215a9cbb5532e4b5c3f0d50
Sami Farin <hvtaifwkbgefbaei@gmail.com>
2026-07-15 11:02:28 +0300

    mix_free_type2_list : free correct ptr

--- remailer.c
+++ remailer.c
@@ -221,7 +221,7 @@ static void mix_free_type2_list(REMAILER ***ttlp)
   for (i = 0; type2_list[i]; i++)
     mix_free_remailer(&type2_list[i]);
 
-  FREE(type2_list);            /* __FREE_CHECKED__ */
+  FREE(ttlp);            /* __FREE_CHECKED__ */
 }
 
 
