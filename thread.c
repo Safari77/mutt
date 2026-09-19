baa7da0a638332293eeae60bb10aef396e4feb8c
Sami Farin <hvtaifwkbgefbaei@gmail.com>
2026-09-19 11:22:27 +0300

    mutt_link_threads uses global Context instead of passed ctx

--- thread.c
+++ thread.c
@@ -1648,8 +1648,8 @@ int mutt_link_threads(HEADER *cur, HEADER *last, CONTEXT *ctx)
   if (!last)
   {
     for (i = 0; i < ctx->vcount; i++)
-      if (ctx->hdrs[Context->v2r[i]]->tagged)
-        changed |= link_threads(cur, ctx->hdrs[Context->v2r[i]], ctx);
+      if (ctx->hdrs[ctx->v2r[i]]->tagged)
+        changed |= link_threads(cur, ctx->hdrs[ctx->v2r[i]], ctx);
   }
   else
     changed = link_threads(cur, last, ctx);
